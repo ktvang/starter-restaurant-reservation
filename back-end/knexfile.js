a2e79076bd87cd0153f1e293e39ea7d614461f2b
ktvang <38084371+ktvang@users.noreply.github.com>
2024-05-07 18:54:09 -0600

    Commit

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://ypbzzunt:sKjTWTFYl6O-4RcRxLa8jt6EL8DI2AvA@bubble.db.elephantsql.com/ypbzzunt",
+  DATABASE_URL_DEVELOPMENT = "postgres://yutgizpy:1vcY260znfk7tcI7DobwJrNVindRGcMm@kala.db.elephantsql.com/yutgizpy",
+  DATABASE_URL_TEST = "postgres://zitclqra:ynqwReOYASnJ6TK-4QdNQwusWjVZrD9m@kala.db.elephantsql.com/zitclqra",
+  DATABASE_URL_PREVIEW = "postgres://fepyzcap:WX9iOhYBv-KgsPz30kQHEO3_Nuhb6atX@kala.db.elephantsql.com/fepyzcap",
   DEBUG,
 } = process.env;
 
--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const controller = require("./reservations.controller");
+const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
@@ -9,14 +9,9 @@ router
   .all(methodNotAllowed);
 
 router
-  .route("/:reservation_id")
-  .get(controller.read)
+  .route("/:table_id/seat")
   .put(controller.update)
-  .all(methodNotAllowed);
-
-router
-  .route("/:reservation_id/status")
-  .put(controller.updateStatus)
+  .delete(controller.finish)
   .all(methodNotAllowed);
 
 module.exports = router;
